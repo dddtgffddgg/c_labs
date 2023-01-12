c768a6aeed088fec4d1a225f38ceb2e86f06b99a
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-01-12 14:39:25 +0500

    Create laba8.c
    неуверенно

---
+++ laba8.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+
+int main ()
+{
+int c;
+
+    while (1)
+{
+    c = mygetch();
+    if (c == 27)
+    continue;
+    if (c == 0)
+{
+    c = mygetch ();
+    if (c >= 59 && c <= 65 || c == 67 || c == 68)
+{
+    continue;
+}
+    if (c == 66)
+    break;
+}
+    if (c == 224)
+{
+    c = mygetch ();
+    if (c == 133 || c == 134)
+{
+    continue;
+}
+    if (c >= 80 && c <= 83)
+{
+    continue;
+}
+    if (c >= 71 && c <= 73 || c == 75 || c == 77 || c == 79)
+{
+    continue;
+}
+}
+    printf ("%c", toupper (c));
+}
+    return 0;
+}
