236be3c2dba19933f830c209ac3c45b6eee2b55f
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-01-12 16:53:51 +0500

    Update and rename laba11.c to laba1.c

--- laba1.c
+++ laba1.c
@@ -9,19 +9,19 @@ int main()
     printf("Введите z ->");
     scanf("%f", &z);
     float max, else1, else2;
-    if ( x > y && x > z)
+    if ( x >= y && x >= z)
     {
         max = x;
         else1 = y;
         else2 = z;
     }
-    if ( y > x && y > z)
+    if ( y >= x && y >= z)
     {
         max = y; 
         else1 = x;
         else2 = z;
     }
-    if ( z > x && z > y)
+    if ( z >= x && z >= y)
     {
         max = z;
         else1 = x;
@@ -36,4 +36,5 @@ int main()
     }
     else
     printf("%lf", g);
+  return 0;
 }
